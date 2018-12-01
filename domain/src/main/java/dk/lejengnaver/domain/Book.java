a53a1b6d140fc6f525f37c3cd7cf5d3cfaa5c9d1
Michael Broe <michaelbroe@gmail.com>
2018-12-01 21:07:30 +0100

    Create Book.java

---
+++ domain/src/main/java/dk/lejengnaver/domain/Book.java
@@ -0,0 +1,15 @@
+package dk.lejengnaver.domain;
+
+import java.util.Date;
+
+public class Book {
+
+    private String name;
+	
+	private String category;
+	
+	private Date published; 
+	
+	private Integer pages;
+
+}

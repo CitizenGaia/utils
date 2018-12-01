267c571178dbc40502f36428c7d5326931111b0c
Michael Broe <michaelbroe@gmail.com>
2018-12-01 20:35:26 +0100

    Create Json2XsdUtilTest.java

---
+++ store/src/test/dk/lejengnaver/utils/Json2XsdUtilTest.java
@@ -0,0 +1,12 @@
+package dk.lejengnaver.utils;
+
+import org.junit.*;
+
+public class Json2XsdUtilTest {
+
+   @Test
+   public void testExampleson() {	
+      System.out.println("Inside testExampleson()");
+	}
+
+}

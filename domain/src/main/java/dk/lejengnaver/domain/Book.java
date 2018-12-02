90a91c065e5c86f35c4bd9860f90946f014c5fea
Michael Broe <michaelbroe@gmail.com>
2018-12-02 19:25:35 +0100

    Update Book.java

--- domain/src/main/java/dk/lejengnaver/domain/Book.java
+++ domain/src/main/java/dk/lejengnaver/domain/Book.java
@@ -7,10 +7,42 @@ public class Book {
 
     private String name;
 	
-	private String category;
+    private String category;
 	
-	private Date published; 
+    private Date published; 
 	
-	private Integer pages;
+    private Integer pages;
+	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
 
 }

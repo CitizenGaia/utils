f7b3e6da2741e84c43c766aeaa87c0b4aa0700c8
Michael Broe <michaelbroe@gmail.com>
2018-12-02 18:29:05 +0100

    Update Json2XsdUtilTest.java

--- store/src/test/dk/lejengnaver/utils/Json2XsdUtilTest.java
+++ store/src/test/dk/lejengnaver/utils/Json2XsdUtilTest.java
@@ -4,9 +4,29 @@ import org.junit.*;
 
 public class Json2XsdUtilTest {
 
-   @Test
-   public void testExampleson() {	
-      System.out.println("Inside testExampleson()");
-	}
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        System.out.println("@BeforeClass");
+    }
+
+    @AfterClass
+    public static void runOnceAfterClass() {
+        System.out.println("@AfterClass");
+    }
+
+    @Before
+    public void runBeforeTestMethod() {
+        System.out.println("@Before");
+    }
+
+    @After
+    public void runAfterTestMethod() {
+        System.out.println("@After");
+    }
+	
+    @Test
+    public void testExampleson() {	
+        System.out.println("Inside testExampleson()");
+    }
 
 }
